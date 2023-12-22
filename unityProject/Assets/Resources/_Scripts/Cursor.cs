dfb33a860ea0c94100b1d320d6991e6faa45ec0d
FerCamarena <fernando.camarena6817@alumnos.udg.mx>
2023-12-22 15:48:31 -0600

    Bufixing and visual updates
    Modified drag and drop behaviours.
    Bugfixed drag and drop while not hovering over items.
    Deleted previous shaders and materials related to hovering.

--- unityProject/Assets/Resources/_Scripts/Cursor.cs
+++ unityProject/Assets/Resources/_Scripts/Cursor.cs
@@ -2,31 +2,17 @@ using UnityEngine;
 public class Cursor : MonoBehaviour {
     private GameObject selectedItem;
     private Vector3 cursorPoint;
-    [SerializeField] private Material hoverMaterial;
+    private Vector3 cursorOffset;
     Item selection;
     private void Update() {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Input.GetMouseButtonDown(0)) {
-            if (Physics.Raycast(ray, out hit)) {
-                if(!this.selectedItem) {
-                    this.selectedItem = hit.collider.gameObject;
-                    this.selectedItem.TryGetComponent<Item>(out selection);
-                    if(!this.selection || !this.selection.selected) { 
-                        this.selection.selected = true;
-                    }
-                }
-                if (this.selectedItem.CompareTag("Item")) {
-                    Material[] originalMaterials = this.selectedItem.GetComponent<MeshRenderer>().sharedMaterials;
-                    Material[] newMaterials = new Material[originalMaterials.Length + 1];
-
-                    for (int i = 0; i < originalMaterials.Length; i++) {
-                        newMaterials[i] = originalMaterials[i];
-                    }
-
-                    newMaterials[originalMaterials.Length] = this.hoverMaterial;
-
-                    this.selectedItem.GetComponent<MeshRenderer>().sharedMaterials = newMaterials;
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit)) {
+            if(!this.selectedItem && hit.collider.gameObject.CompareTag("Item")) {
+                this.selectedItem = hit.collider.gameObject;
+                this.cursorOffset = this.selectedItem.transform.position - hit.point;
+                if(this.selectedItem.TryGetComponent<Item>(out this.selection) && (!this.selection || !this.selection.selected)) { 
+                    this.selection.selected = true;
                 }
             }
         }else if(Input.GetMouseButton(0) && this.selectedItem) {
@@ -36,24 +22,17 @@ public class Cursor : MonoBehaviour {
                 mousePosition.y = Mathf.Clamp(mousePosition.y, 0f, Screen.height);
                 mousePosition.z = Camera.main.transform.position.y * 0.9f;
                 this.cursorPoint = Camera.main.ScreenToWorldPoint(mousePosition);
-                this.selectedItem.transform.position = this.cursorPoint;
+                this.selectedItem.transform.position = this.cursorPoint + this.cursorOffset;
                 //Vector3 cursorDirection = (this.cursorPoint - selectedItem.transform.position).normalized;
                 //selectedItem.transform.zposition = Quaternion.LookRotation(cursorDirection);
             }
         } else if(Input.GetMouseButtonUp(0)) {
-            Material[] originalMaterials = this.selectedItem.GetComponent<MeshRenderer>().sharedMaterials;
-            Material[] newMaterials = new Material[originalMaterials.Length-2];
-
-            for (int i = 0; i < originalMaterials.Length-2; i++) {
-                //SI EL MATERIAL == HOVER NO LO AGREGA
-                newMaterials[i] = originalMaterials[i];
+            if (this.selectedItem) { 
+                this.selectedItem = null;
             }
-            //FALTA CAMBIAR EL MATERIAL POR UNICAMENTE REMPLAZAR EL QUE SE USA PARA HOVER NO EL ULTIMO.
-            //originalMaterials[originalMaterials.Length-1] = null;
-            this.selectedItem.GetComponent<MeshRenderer>().sharedMaterials = newMaterials;
-
-            this.selectedItem = null;
-            this.selection.selected = false;
+            if (this.selection) { 
+                this.selection.selected = false;
+            }
         }
     }
 }
