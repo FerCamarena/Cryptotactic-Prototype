8cb78f70419fbb67695687e0b53f7afed27b9c74
FerCamarena <fernando.camarena6817@alumnos.udg.mx>
2023-12-22 19:13:09 -0600

    Items visuals setup
    Added method for controlling hover, dragging and selection outline materials on items.
    Modified camera mode and distance from environment.
    Added boolean values to determinate which objects are being hover over, selected or being dragged.

--- unityProject/Assets/Resources/_Scripts/Cursor.cs
+++ unityProject/Assets/Resources/_Scripts/Cursor.cs
@@ -7,31 +7,36 @@ public class Cursor : MonoBehaviour {
     private void Update() {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit)) {
-            if(!this.selectedItem && hit.collider.gameObject.CompareTag("Item")) {
+        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.CompareTag("Item")) {
+            if (Input.GetMouseButtonDown(0)) {
                 this.selectedItem = hit.collider.gameObject;
                 this.cursorOffset = this.selectedItem.transform.position - hit.point;
-                if(this.selectedItem.TryGetComponent<Item>(out this.selection) && (!this.selection || !this.selection.selected)) { 
-                    this.selection.selected = true;
+                if(this.selectedItem.TryGetComponent<Item>(out this.selection)) {
+                    this.selection.hovered = false;
+                }
+            } else if (!Input.GetMouseButton(0)) {
+                if (hit.collider.gameObject.CompareTag("Item") && hit.collider.TryGetComponent<Item>(out this.selection)){
+                    this.selection.hovered = true;
                 }
             }
-        }else if(Input.GetMouseButton(0) && this.selectedItem) {
-            if (this.selectedItem.CompareTag("Item")) {
-                Vector3 mousePosition = Input.mousePosition;
-                mousePosition.x = Mathf.Clamp(mousePosition.x, 0f, Screen.width);
-                mousePosition.y = Mathf.Clamp(mousePosition.y, 0f, Screen.height);
-                mousePosition.z = Camera.main.transform.position.y * 0.9f;
-                this.cursorPoint = Camera.main.ScreenToWorldPoint(mousePosition);
-                this.selectedItem.transform.position = this.cursorPoint + this.cursorOffset;
-                //Vector3 cursorDirection = (this.cursorPoint - selectedItem.transform.position).normalized;
-                //selectedItem.transform.zposition = Quaternion.LookRotation(cursorDirection);
-            }
-        } else if(Input.GetMouseButtonUp(0)) {
-            if (this.selectedItem) { 
-                this.selectedItem = null;
-            }
-            if (this.selection) { 
-                this.selection.selected = false;
+        }
+        if(Input.GetMouseButton(0) && this.selectedItem) {
+            this.selection.dragged = true;
+            this.selection.hovered = false;
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.x = Mathf.Clamp(mousePosition.x, 0f, Screen.width);
+            mousePosition.y = Mathf.Clamp(mousePosition.y, 0f, Screen.height);
+            mousePosition.z = Camera.main.transform.position.y * 0.99f;
+            this.cursorPoint = Camera.main.ScreenToWorldPoint(mousePosition);
+            this.selectedItem.transform.position = this.cursorPoint + this.cursorOffset;
+            //Vector3 cursorDirection = (this.cursorPoint - selectedItem.transform.position).normalized;
+            //selectedItem.transform.zposition = Quaternion.LookRotation(cursorDirection);
+        }
+        if(Input.GetMouseButtonUp(0)) {
+            this.selectedItem = null;
+            if (this.selection) {
+                this.selection.hovered = false;
+                this.selection.dragged = false;
             }
         }
     }
--- unityProject/Assets/Resources/_Scripts/Item.cs
+++ unityProject/Assets/Resources/_Scripts/Item.cs
@@ -2,10 +2,36 @@ using UnityEngine;
 
 public class Item : MonoBehaviour {
     public bool selected;
-
+    public bool dragged;
+    public bool hovered;
+    public GameObject selection;
+    public GameObject drag;
+    public GameObject hover;
     private void Start() {
     }
-
     private void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit) && hit.collider.gameObject == this.gameObject) {
+            if(Input.GetKeyDown(KeyCode.F)) {
+                selected ^= true;
+            }
+        }
+        if(selected) { 
+            selection.SetActive(true);
+        } else {
+            selection.SetActive(false);
+        }
+        if(dragged) {
+            drag.SetActive(true);
+        } else {
+            drag.SetActive(false);
+        }
+        if(hovered) {
+            hover.SetActive(true);
+        } else {
+            hover.SetActive(false);
+        }
+        hovered = false;
     }
 }
